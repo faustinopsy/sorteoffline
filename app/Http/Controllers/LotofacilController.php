6a8377077cdcbc5f495d4d2873c39724ea9c74db
faustinopsy <rodrigohipnose@gmail.com>
2022-12-22 20:35:43 -0300

    iniciando conversão de sistema php puro para laravel

---
+++ app/Http/Controllers/LotofacilController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LotofacilController extends Controller
+{
+    //
+}
---
+++ app/Models/Lotofacil.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lotofacil extends Model
+{
+    use HasFactory;
+}
