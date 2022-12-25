6b508347cd08c74a4d636676a2494367220dccd3
faustinopsy <rodrigohipnose@gmail.com>
2022-12-25 20:25:31 -0300

    megasena e quina em andamento

--- app/Http/Controllers/LotofacilController.php
+++ app/Http/Controllers/LotofacilController.php
@@ -11,29 +11,70 @@ class LotofacilController extends Controller
     {
         return view('lotofacil/index');
     }
-    public function api(){
+    public function repetidosltf(){
         $start_from = 0;
-        $limit = 2;
+        $limit = 5;
         $novo=array();
         $lotofacil = Lotofacil::select('B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12','B13','B14','B15')
         ->orderBy('Concurso','DESC')
-        ->skip(0)
-        ->take(5)
-        ->get();
-      
-        foreach($lotofacil as $key=> $value){
-           echo $key->$value;
-        }
-      
-        //return Response()->json($novo);
-      }
-      public function repetidos(){
-        $start_from = 0;
-        $limit = 5;
-        $lotofacil = Lotofacil::orderBy('id','DESC')
         ->skip($start_from)
         ->take($limit)
         ->get();
-        return Response()->json($lotofacil);
+      foreach($lotofacil as $key=> $value){
+        $novo[]=$lotofacil[$key]->B1;
+        $novo[]=$lotofacil[$key]->B2;
+        $novo[]=$lotofacil[$key]->B3;
+        $novo[]=$lotofacil[$key]->B4;
+        $novo[]=$lotofacil[$key]->B5;
+        $novo[]=$lotofacil[$key]->B6;
+        $novo[]=$lotofacil[$key]->B7;
+        $novo[]=$lotofacil[$key]->B8;
+        $novo[]=$lotofacil[$key]->B9;
+        $novo[]=$lotofacil[$key]->B10;
+        $novo[]=$lotofacil[$key]->B11;
+        $novo[]=$lotofacil[$key]->B12;
+        $novo[]=$lotofacil[$key]->B13;
+        $novo[]=$lotofacil[$key]->B14;
+        $novo[]=$lotofacil[$key]->B15;
+      }
+      arsort($novo);
+        return Response()->json(array_count_values($novo));
+      }
+      public function sorteiosltf(Request $request){
+        $start_from = 0;
+        $limit = 1;
+        $novo=array();
+        $lotofacil = Lotofacil::select('Concurso','B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12','B13','B14','B15')
+        ->where('Data',$request->input('data'))
+        ->orderBy('Concurso','DESC')
+        ->skip($start_from)
+        ->take($limit)
+        ->get();
+        foreach($lotofacil as $key=> $value){
+          $novo[]=$lotofacil[$key]->Concurso;
+          $novo[]=$lotofacil[$key]->B1;
+          $novo[]=$lotofacil[$key]->B2;
+          $novo[]=$lotofacil[$key]->B3;
+          $novo[]=$lotofacil[$key]->B4;
+          $novo[]=$lotofacil[$key]->B5;
+          $novo[]=$lotofacil[$key]->B6;
+          $novo[]=$lotofacil[$key]->B7;
+          $novo[]=$lotofacil[$key]->B8;
+          $novo[]=$lotofacil[$key]->B9;
+          $novo[]=$lotofacil[$key]->B10;
+          $novo[]=$lotofacil[$key]->B11;
+          $novo[]=$lotofacil[$key]->B12;
+          $novo[]=$lotofacil[$key]->B13;
+          $novo[]=$lotofacil[$key]->B14;
+          $novo[]=$lotofacil[$key]->B15;
+        }
+        asort($novo);
+        return view('/lotofacil/sorteios-lotofacil', array('lotofacil' => $novo));
+        //return Response()->json($lotofacil);
+      }
+      public function combinacoesltf(Request $request){
+        
+        //return view('/lotofacil/combinacoesltf', array('lotofacil' => $novo));
+        return view('/lotofacil/combinacoesltf');
       }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,25 +21,47 @@ Route::get('/', function () {
 });
 
 Route::get('/lotofacil', [LotofacilController::class, 'index'])->name('index');
-Route::get('/api', [LotofacilController::class, 'api'])->name('api');
-
+Route::get('/repetidosltf', [LotofacilController::class, 'repetidosltf'])->name('repetidosltf');
+Route::post('/sorteios-lotofacil', [LotofacilController::class, 'sorteiosltf'])->name('lotofacil.sorteio');
 Route::get('/ganhadores-lotofacil', function () {
     return view('lotofacil/ganhadores-lotofacil');
 });
 Route::get('/repetidos-lotofacil', function () {
     return view('lotofacil/repetidos-lotofacil');
 });
-//Route::get('/repetidos-lotofacil', [LotofacilController::class, 'repetidos'])->name('repetidos');
+Route::get('/repetidos-lotofacil/{concurso}', function () {
+    return view('lotofacil/sorteio');
+});
 
+Route::post('/combinacoes-lotofacil', [LotofacilController::class, 'combinacoesltf'])->name('lotofacil.combinacoesltf');
 Route::get('/sorteios-lotofacil', function () {
     return view('lotofacil/sorteios-lotofacil');
 });
 Route::get('/combinacoes-lotofacil', function () {
     return view('lotofacil/combinacoes-lotofacil');
 });
+
 Route::get('/megasena', [MegasenaController::class, 'index'])->name('index');
+Route::get('/repetidosmg', [MegasenaController::class, 'repetidosmg'])->name('repetidosmg');
+Route::get('/sorteios-megasena', function () {
+    return view('megasena/sorteios-megasena');
+});
+Route::post('/sorteios-megasena', [MegasenaController::class, 'sorteiosmg'])->name('megasena.sorteio');
+Route::get('/ganhadores-megasena', function () {
+    return view('megasena/ganhadores-megasena');
+});
+Route::get('/repetidos-megasena', function () {
+    return view('megasena/repetidos-megasena');
+});
+
 
 Route::get('/quina', [QuinaController::class, 'index'])->name('index');
+Route::get('/repetidosqu', [QuinaController::class, 'repetidosqu'])->name('repetidosqu');
+Route::post('/sorteios-quina', [QuinaController::class, 'sorteiosqu'])->name('quina.sorteio');
+Route::get('/ganhadores-quina', function () {
+    return view('quina/ganhadores-quina');
+});
+
 
 Auth::routes();
 
